4a7e51cdb47453ae92bcb1d1b534f5ad94506251
Darkisda <luandesouzanascimento2011@gmail.com>
2020-08-24 20:29:44 -0300

    Bug fix

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,7 +3,6 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'localhost',
-  port: 50651,
   username: 'postgres',
   password: '2410',
   database: 'nest',
